8466e8483f01a737816bc8dd70af840e74f71099
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2024-08-07 11:40:05 -0300

    preparacion para pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Leer las dependencias del archivo requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='pyppi',
+    version='0.0.1',
+    author='MartinBasualdo0',
+    author_email='martin.basualdo@hotmail.com',
+    description='WebScrap de PortfolioPersonal',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MartinBasualdo0/pyPPI',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+)
